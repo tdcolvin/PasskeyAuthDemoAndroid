4fcef73202717ce635b2b9dfcc3ee565e369ebd0
Tom Colvin <tdcolvin@gmail.com>
2025-03-06 10:38:32 +0000

    Added state machine to show "signed in" screen

--- app/src/main/java/com/tdcolvin/passkeyauthdemo/MainActivity.kt
+++ app/src/main/java/com/tdcolvin/passkeyauthdemo/MainActivity.kt
@@ -13,8 +13,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.credentials.CreatePublicKeyCredentialRequest
@@ -60,8 +63,44 @@ fun PasskeyDemoScreen(
 
     val credentialManager = remember { CredentialManager.create(localContext) }
 
-    val username = remember { String((0..5).map{"abcdefghijklmnopqrstuvwxyz0123456789".random()}.toCharArray()) }
+    val username = remember {
+        String((0..5).map { "abcdefghijklmnopqrstuvwxyz0123456789".random() }.toCharArray())
+    }
+    var signedIn by remember { mutableStateOf(false) }
 
+    if (signedIn) {
+        SignedInScreen(
+            modifier = modifier,
+            username = username,
+            onSignOut = { signedIn = false }
+        )
+    }
+    else {
+        SignedOutScreen(
+            modifier = modifier,
+            username = username,
+            credentialManager = credentialManager,
+            getPasskeyRegisterRequestJson = viewModel::getPasskeyRegisterRequestJson,
+            sendRegistrationResponse = viewModel::sendRegistrationResponse,
+            getPasskeyAuthenticationRequestJson = viewModel::getPasskeyAuthenticationRequestJson,
+            sendAuthenticationResponse = viewModel::sendAuthenticationResponse,
+            onSignedIn = { signedIn = true }
+        )
+    }
+}
+
+@Composable
+fun SignedOutScreen(
+    modifier: Modifier = Modifier,
+    username: String,
+    credentialManager: CredentialManager,
+    getPasskeyRegisterRequestJson: suspend (String) -> String,
+    sendRegistrationResponse: suspend (String) -> String,
+    getPasskeyAuthenticationRequestJson: suspend (String) -> String,
+    sendAuthenticationResponse: suspend (String) -> String,
+    onSignedIn: () -> Unit
+) {
+    var error by remember { mutableStateOf<Exception?>(null) }
     Column(
         modifier = modifier
     ) {
@@ -69,15 +108,37 @@ fun PasskeyDemoScreen(
         SignUpWithPasskey(
             username = username,
             credentialManager = credentialManager,
-            getPasskeyRegisterRequestJson = viewModel::getPasskeyRegisterRequestJson,
-            sendRegistrationResponse = viewModel::sendRegistrationResponse
+            getPasskeyRegisterRequestJson = getPasskeyRegisterRequestJson,
+            sendRegistrationResponse = sendRegistrationResponse,
+            onSignedUp = onSignedIn,
+            onError = { error = it }
         )
         SignInWithPasskey(
             username = username,
             credentialManager = credentialManager,
-            getPasskeyAuthenticationRequestJson = viewModel::getPasskeyAuthenticationRequestJson,
-            sendAuthenticationResponse = viewModel::sendAuthenticationResponse
+            getPasskeyAuthenticationRequestJson = getPasskeyAuthenticationRequestJson,
+            sendAuthenticationResponse = sendAuthenticationResponse,
+            onSignedIn = onSignedIn,
+            onError = { error = it }
         )
+    }
+}
+
+@Composable
+fun SignedInScreen(
+    modifier: Modifier = Modifier,
+    username: String,
+    onSignOut: () -> Unit
+) {
+    Column(modifier = modifier) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = "Successfully signed in as $username"
+        )
+
+        Button(onClick = onSignOut) {
+            Text("Sign out")
+        }
     }
 }
 
@@ -88,7 +149,9 @@ fun SignUpWithPasskey(
     username: String,
     credentialManager: CredentialManager,
     getPasskeyRegisterRequestJson: suspend (String) -> String,
-    sendRegistrationResponse: suspend (String) -> String
+    sendRegistrationResponse: suspend (String) -> String,
+    onSignedUp: () -> Unit,
+    onError: (Exception) -> Unit
 ) {
     val localActivity = LocalActivity.current
     val signUpScope = rememberCoroutineScope()
@@ -97,22 +160,29 @@ fun SignUpWithPasskey(
         localActivity ?: return
 
         signUpScope.launch {
-            val registerRequestJson = getPasskeyRegisterRequestJson(username)
+            try {
+                val registerRequestJson = getPasskeyRegisterRequestJson(username)
 
-            val createPublicKeyCredentialRequest = CreatePublicKeyCredentialRequest(
-                requestJson = registerRequestJson,
-                preferImmediatelyAvailableCredentials = false
-            )
-            val createCredentialResponse = credentialManager.createCredential(
-                context = localActivity,
-                request = createPublicKeyCredentialRequest
-            )
+                val createPublicKeyCredentialRequest = CreatePublicKeyCredentialRequest(
+                    requestJson = registerRequestJson,
+                    preferImmediatelyAvailableCredentials = false
+                )
+                val createCredentialResponse = credentialManager.createCredential(
+                    context = localActivity,
+                    request = createPublicKeyCredentialRequest
+                )
 
-            if (createCredentialResponse !is CreatePublicKeyCredentialResponse) {
-                throw Exception("Incorrect response type")
+                if (createCredentialResponse !is CreatePublicKeyCredentialResponse) {
+                    throw Exception("Incorrect response type")
+                }
+
+                sendRegistrationResponse(createCredentialResponse.registrationResponseJson)
+
+                onSignedUp()
             }
-
-            sendRegistrationResponse(createCredentialResponse.registrationResponseJson)
+            catch (e: Exception) {
+                onError(e)
+            }
         }
     }
 
@@ -130,7 +200,9 @@ fun SignInWithPasskey(
     username: String,
     credentialManager: CredentialManager,
     getPasskeyAuthenticationRequestJson: suspend (String) -> String,
-    sendAuthenticationResponse: suspend (String) -> String
+    sendAuthenticationResponse: suspend (String) -> String,
+    onSignedIn: () -> Unit,
+    onError: (Exception) -> Unit
 ) {
     val localActivity = LocalActivity.current
     val signInScope = rememberCoroutineScope()
@@ -141,28 +213,35 @@ fun SignInWithPasskey(
             signInScope.launch {
                 localActivity ?: return@launch
 
-                val authenticationRequestJson = getPasskeyAuthenticationRequestJson(username)
-                Log.v("passkey", "Authentication request JSON: $authenticationRequestJson")
+                try {
+                    val authenticationRequestJson = getPasskeyAuthenticationRequestJson(username)
+                    Log.v("passkey", "Authentication request JSON: $authenticationRequestJson")
 
-                val getPublicKeyCredentialOption = GetPublicKeyCredentialOption(
-                    requestJson = authenticationRequestJson
-                )
-                val signInRequest = GetCredentialRequest(listOf(getPublicKeyCredentialOption))
-                
-                val result = credentialManager.getCredential(
-                    context = localActivity,
-                    request = signInRequest
-                )
+                    val getPublicKeyCredentialOption = GetPublicKeyCredentialOption(
+                        requestJson = authenticationRequestJson
+                    )
+                    val signInRequest = GetCredentialRequest(listOf(getPublicKeyCredentialOption))
 
-                val credential = result.credential
+                    val result = credentialManager.getCredential(
+                        context = localActivity,
+                        request = signInRequest
+                    )
 
-                if (credential !is PublicKeyCredential) {
-                    throw Exception("Incorrect credential type")
+                    val credential = result.credential
+
+                    if (credential !is PublicKeyCredential) {
+                        throw Exception("Incorrect credential type")
+                    }
+
+                    val responseJson = credential.authenticationResponseJson
+
+                    sendAuthenticationResponse(responseJson)
+
+                    onSignedIn()
                 }
-
-                val responseJson = credential.authenticationResponseJson
-
-                sendAuthenticationResponse(responseJson)
+                catch (e: Exception) {
+                    onError(e)
+                }
             }
         }
     ) {
